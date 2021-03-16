c11a59c50542c2bbc0ec374cd83513d8208080a5
Gray-Yang <59634081+Gray-Yang@users.noreply.github.com>
2021-03-16 13:07:20 -0400

    Add files via upload

---
+++ ZS 3D 2.00 WMH109/EVYG_3DPath/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Health Indicator
+//
+//  Created by Dongdong on 2013-07-25.
+//  Copyright (c) 2013 evanyanagi. All rights reserved.
+//
+//  v1.05  added compass
+//  v1.06  iOS 8 support
+//  v1.07  fix traditional chinese 'олЁь', update acquatance and erratic cards
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EVYGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EVYGAppDelegate class]));
+    }
+}
